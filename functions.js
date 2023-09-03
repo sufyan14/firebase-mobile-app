82e2c52ce04a4dffc91cc9a4f08ac28e87525081
Muhammad Sufyan <muhammadsufyan00ms@gmail.com>
2023-09-03 22:43:43 +0500

    fetched data from firebase

---
+++ functions.js
@@ -0,0 +1,21 @@
+  // Practice 
+
+  // let UserEmails = {
+  //   "00": "abc@gmail.com",
+  //   "01": "123@gmail.com",
+  //   "02": "yoyo@gmail.com"
+  // }
+  // let UserIDs = {
+  //   "00": "abc@gmail.com",
+  //   "01": "123@gmail.com",
+  //   "02": "yoyo@gmail.com"
+  // }
+  // let UserEntries = {
+  //   "00": "abc@gmail.com",
+  //   "01": "123@gmail.com",
+  //   "02": "yoyo@gmail.com"
+  // }
+
+  // console.log(Object.values(UserEmails))
+  // console.log(Object.keys(UserIDs))
+  // console.log(Object.entries(UserEntries))
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -16,10 +17,34 @@ const itemsInDB = ref(database, "items");
 
 const item = document.getElementById("input-field");
 const add_button = document.getElementById("add-button");
+const groceryList = document.getElementById("grocery-list");
 
 add_button.addEventListener("click", function () {
   let inputVal = item.value;
   push(itemsInDB, inputVal);
 
-  console.log(`${inputVal} added to database`);
-});
+  clearItem();
+})
+ 
+onValue(itemsInDB, function(snapshot) {
+  let itemsArray = Object.values(snapshot.val())
+  
+  clearGroceryItem();
+  for (let i = 0; i < itemsArray.length; i++) {
+
+    appendItem(itemsArray[i])
+  }
+})
+
+
+function appendItem(inputVal) {
+    groceryList.innerHTML += `<li>${inputVal}</li>`;
+}
+
+function clearGroceryItem() {
+  groceryList.innerHTML = ""
+}
+
+function clearItem() {
+    item.value = "";
+}
